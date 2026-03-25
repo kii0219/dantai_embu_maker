a44ce5a30e4419cffa9d815d9bb440916c94fd1f
kii0219 <kii.s.w.t@gmail.com>
2026-03-25 14:12:47 +0900

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "148399949"
+    serviceWorkerVersion: "2482483174"
   }
 });
